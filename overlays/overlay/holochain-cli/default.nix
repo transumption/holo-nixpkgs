4e85e457e4b87d80e1a79ac9e496d8abc875e9f3
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-05-20 08:04:35 +0300

    holochain-cli: clean up

--- overlays/overlay/holochain-cli/default.nix
+++ overlays/overlay/holochain-cli/default.nix
@@ -1,23 +1,22 @@
-{ pkgs, stdenv, fetchurl, openssl }:
+{ stdenv, fetchurl, autoPatchelfHook }:
 
-stdenv.mkDerivation {
-  name = "holochain-cli";
+stdenv.mkDerivation rec {
+  name = "holochain-cli-${version}";
+  version = "0.0.12-alpha1";
 
   src = fetchurl {
-    url = https://github.com/holochain/holochain-rust/releases/download/v0.0.12-alpha1/cli-v0.0.12-alpha1-x86_64-generic-linux-gnu.tar.gz;
+    url = "https://github.com/holochain/holochain-rust/releases/download/v${version}/cli-v${version}-x86_64-generic-linux-gnu.tar.gz";
     sha256 = "15frnjn3q4mfsg53dy59mwnkhzwkf6iwm0d5jix2d575i8cyn5xi";
   };
-  #buildInputs = [
-  #  openssl
-  #];
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+
   installPhase = ''
-    mkdir -p $out/bin
-    cp hc $out/bin
-    patchelf --set-interpreter \
-        ${stdenv.glibc}/lib/ld-linux-x86-64.so.2  $out/bin/hc
-    patchelf --set-rpath  ${stdenv.glibc}/lib $out/bin/hc
-    patchelf --set-rpath  ${openssl.out}/lib $out/bin/hc
-    #patchelf --add-needed ${openssl.out}/lib/libssl.so.1.0.0 $out/bin/hc
-    #patchelf --add-needed ${openssl.out}/lib/libcrypto.so.1.0.0 $out/bin/hc
+    install -Dt $out/bin hc
   '';
+
+  meta = with stdenv.lib; {
+    license = licenses.gpl3;
+    platforms = [ "x86_64-linux" ];
+  };
 }

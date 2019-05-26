fef5eca3c57d50087687c1d9216b0b48ce25a051
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-05-26 11:06:22 +0300

    nixpkgs: pin to 28b4f7b6ace4d7854294f96c190a4b6379136271
    https://github.com/NixOS/nixpkgs-channels/tree/nixos-unstable

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/20fc92f7937fcc21077747e5b614d90a81ed7787.tar.gz";
-  sha256 = "10a1wfmkyzvc47sbd03w39cmlr7wwjy06bs323mlnks5a33ifx7h";
+  url = "https://github.com/NixOS/nixpkgs/archive/28b4f7b6ace4d7854294f96c190a4b6379136271.tar.gz";
+  sha256 = "15wgf7yz9jv3wrsp6hbh0bjbwnd0h2siisjyy1ma5l8bwx2dr8l1";
 })

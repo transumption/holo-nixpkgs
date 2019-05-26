53dcfe7a30eff8b4d1bd34e8b401f9ca6e56f97c
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-05-26 14:44:18 +0300

    holo-cli: bump to dd5e25b1d919abfd86b355d29f9f29ab293ff452

--- overlays/overlay/holo-cli/default.nix
+++ overlays/overlay/holo-cli/default.nix
@@ -6,15 +6,14 @@ buildGoPackage rec {
   src = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "holo-cli";
-    rev = "6a8fd11370fff45fdb763198a67cb79c86e1efe3";
-    sha256 = "12rck2d1gz56y69wycy2jf7dp9qaf47bdi6gfb15kr3rarigvy69";
+    rev = "dd5e25b1d919abfd86b355d29f9f29ab293ff452";
+    sha256 = "1wh2hi7zfs3dnp1xaw11dd7rr6xqq1973bghba8pp4sv6wmvy888";
   };
 
   goDeps = ./deps.nix;
   goPackagePath = "github.com/Holo-Host/holo-cli";
 
   meta = with stdenv.lib; {
-    homepage = "https://github.com/Holo-Host/holo-cli";
     license = licenses.asl20;
     platforms = platforms.unix;
   };

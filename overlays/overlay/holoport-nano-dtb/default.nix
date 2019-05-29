6322b1081a2565cdd22cee4e0bb5882ad854833f
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-05-29 23:04:28 +0300

    holoport-nano-dtb: fix cross-compilation

--- overlays/overlay/holoport-nano-dtb/default.nix
+++ overlays/overlay/holoport-nano-dtb/default.nix
@@ -1,10 +1,17 @@
 { stdenv, fetchpatch, dtc, linux }:
 
+let
+  cpp = if stdenv.buildPlatform == stdenv.hostPlatform
+    then "cpp"
+    else "${stdenv.hostPlatform.config}-cpp";
+in
+
 stdenv.mkDerivation {
   name = "${linux.name}-holoport-nano-dtb";
   inherit (linux) src;
 
   nativeBuildInputs = [ dtc ];
+
   patches = [
     (fetchpatch {
       url = "https://lkml.org/lkml/diff/2019/5/26/40/1";
@@ -17,7 +24,7 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/allwinner
 
-    cpp -nostdinc -I include -x assembler-with-cpp \
+    ${cpp} -nostdinc -I include -x assembler-with-cpp \
       arch/arm64/boot/dts/allwinner/sun50i-a64-bananapi-m64.dts \
       | dtc > $out/allwinner/sun50i-a64-bananapi-m64.dtb
   '';

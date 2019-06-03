986cededb38fe8a5ebcc6e4a765364b5a3a8c447
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-06-04 02:30:25 +0300

    Revert "treewide: fix Hydra eval"
    This reverts commit 6b6497c1fde9c143be16be83948d950670f27d11.

--- default.nix
+++ default.nix
@@ -1,13 +1,15 @@
 { pkgs ? import ./nixpkgs {} }: with pkgs;
 
 {
-  artifacts.installers = {
-    holoport = import ./artifacts/installers/holoport { inherit pkgs; };
-    holoport-nano = import ./artifacts/installers/holoport-nano { inherit pkgs; };
-    holoport-plus = import ./artifacts/installers/holoport-plus { inherit pkgs; };
+  artifacts = recurseIntoAttrs {
+    installers = recurseIntoAttrs {
+      holoport = import ./artifacts/installers/holoport { inherit pkgs; };
+      holoport-nano = import ./artifacts/installers/holoport-nano { inherit pkgs; };
+      holoport-plus = import ./artifacts/installers/holoport-plus { inherit pkgs; };
+    };
   };
 
-  tests = {
+  tests = recurseIntoAttrs {
     boot = import ./tests/boot.nix { inherit pkgs; };
   };
 }

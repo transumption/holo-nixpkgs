56c797e07d69123158769304d02d3e7ac225482f
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-06-11 22:48:13 +0300

    nixpkgs: address https://git.io/fjgMn

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -9,14 +9,9 @@ import (stdenvNoCC.mkDerivation {
   src = nixpkgs;
 
   patches = [
-    # Fix services.ssh during cross-compilation
-    (fetchpatch {
-      url = "https://github.com/NixOS/nixpkgs/commit/e5c3364c7bf494fd46ddfb41ecae2b9718a141ac.patch";
-      sha256 = "1sbwpffg0777jcc4ffs9vgc7rn443rvnb1f7ap0nayq0ajjlf1a0";
-    })
-
     ./ext4-no-resize2fs.diff
     ./kmod-cross.diff
+    ./nixos-ssh-cross.diff
   ];
 
   installPhase = ''

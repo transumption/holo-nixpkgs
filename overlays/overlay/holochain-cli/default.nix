0b828db2df4414ed1483ecfaf6b4f29995a648a2
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-07-02 15:26:56 +0300

    holochain-cli: update to 0.0.18-alpha1

--- overlays/overlay/holochain-cli/default.nix
+++ overlays/overlay/holochain-cli/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "holochain-cli-${version}";
-  version = "0.0.12-alpha1";
+  version = "0.0.18-alpha1";
 
   src = fetchurl {
     url = "https://github.com/holochain/holochain-rust/releases/download/v${version}/cli-v${version}-x86_64-generic-linux-gnu.tar.gz";
-    sha256 = "15frnjn3q4mfsg53dy59mwnkhzwkf6iwm0d5jix2d575i8cyn5xi";
+    sha256 = "1imwbns45d4k3j4ra7swbd74zhh4kqjq4i8y8qmkl63rflcvpkia";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];

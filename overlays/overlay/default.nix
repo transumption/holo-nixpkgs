545002acf9feaf19ff61530909ac2e63a86b259e
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-09 13:29:01 +0000

    Revert "profiles/hardware/packet: init iSCSI block storage support"
    This reverts commit 1683a6c66c8b0e18c630ca47d2ebbde5052caea0.

--- overlays/overlay/default.nix
+++ overlays/overlay/default.nix
@@ -50,6 +50,4 @@ final: previous: with final;
     url = "https://github.com/samrose/n3h/archive/4c3541a7432b4f0ac72cb3dac83affc062f055e1.tar.gz";
     sha256 = "1cbdjjg0nxm4bwvw7l6srhlll1iiz2il1kggvkhbh7c0c830fr2w";
   }) {}).package;
-
-  packet-block-storage = callPackage ./packet-block-storage {};
 }

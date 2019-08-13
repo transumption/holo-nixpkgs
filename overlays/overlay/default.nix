2c90020c871a9d8572f666a3b4fc3abceda78693
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-13 11:15:38 +0000

    n3h: drop

--- overlays/overlay/default.nix
+++ overlays/overlay/default.nix
@@ -51,10 +51,5 @@ in
     sun50i-a64-gpadc-iio = self.callPackage ./sun50i-a64-gpadc-iio {};
   });
 
-  n3h = (callPackage (fetchzip {
-    url = "https://github.com/samrose/n3h/archive/4c3541a7432b4f0ac72cb3dac83affc062f055e1.tar.gz";
-    sha256 = "1cbdjjg0nxm4bwvw7l6srhlll1iiz2il1kggvkhbh7c0c830fr2w";
-  }) {}).package;
-
   inherit (callPackage npm-to-nix {}) npmToNix;
 }

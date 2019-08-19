59041070d32b37da514182016abf3e70097b184c
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-19 13:35:03 +0000

    profiles/builders/hydra: write NAR listing

--- profiles/builders/hydra/default.nix
+++ profiles/builders/hydra/default.nix
@@ -33,7 +33,7 @@
   services.hydra.extraConfig = ''
     binary_cache_public_uri = https://cache.holo.host
     server_store_uri = https://cache.holo.host?local-nar-cache=/var/cache/hydra/nar-cache
-    store_uri = s3://holo-cache?endpoint=s3.us-west-1.wasabisys.com&log-compression=br&parallel-compression=1&secret-key=/var/lib/hydra/queue-runner/keys/cache.holo.host-1/secret
+    store_uri = s3://holo-cache?endpoint=s3.us-west-1.wasabisys.com&log-compression=br&ls-compression=br&parallel-compression=1&secret-key=/var/lib/hydra/queue-runner/keys/cache.holo.host-1/secret&write-nar-listing=1
     upload_logs_to_binary_cache = true
   '';
 

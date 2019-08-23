c24eb6c618bc7f30b9c8ed394147ac09d73188d2
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-23 12:43:33 +0000

    profiles/targets/demo: add SSH key for Yegor Timoshenko

--- profiles/targets/demo/default.nix
+++ profiles/targets/demo/default.nix
@@ -1,3 +1,8 @@
 {
   imports = [ ../. ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    # Yegor Timoshenko
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLGgzH3ROvo65cnvkXmuz7Qc9bPvU+L2SrafQh0bMrK"
+  ];
 }

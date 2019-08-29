dd0be8b51c485242e3751d4ae315b2059b57fc91
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-29 12:11:51 +0000

    profiles/builders: move binary cache setup to hydra profile

--- profiles/builders/hydra/default.nix
+++ profiles/builders/hydra/default.nix
@@ -34,6 +34,15 @@
     max_connections = 1024
   '';
 
+  services.hydra.extraConfig = ''
+    binary_cache_public_uri = https://cache.holo.host
+    log_prefix = https://holo.s3.wasabisys.com/
+    max_output_size = 17179869184
+    server_store_uri = https://holo.s3.wasabisys.com?local-nar-cache=/var/cache/hydra/nar-cache
+    store_uri = s3://holo?endpoint=s3.wasabisys.com&log-compression=br&ls-compression=br&parallel-compression=1&secret-key=/var/lib/hydra/queue-runner/keys/cache.holo.host-1/secret&write-nar-listing=1
+    upload_logs_to_binary_cache = true
+  '';
+
   services.nginx = {
     # HoloPortOS points to https://cache.holo.host for binary cache. It points
     # to a domain that we control so that we have flexibility to move the bucket
@@ -57,4 +66,9 @@
       serverName = "holoportbuild.holo.host";
     };
   };
+
+  systemd.tmpfiles.rules = [
+    "d /var/cache/hydra           0755 hydra hydra -  -"
+    "d /var/cache/hydra/nar-cache 0775 hydra hydra 1d -"
+  ];
 }
--- profiles/builders/master.nix
+++ profiles/builders/master.nix
@@ -10,14 +10,6 @@
 
   services.hydra = {
     enable = true;
-    extraConfig = ''
-      binary_cache_public_uri = https://cache.holo.host
-      log_prefix = https://holo.s3.wasabisys.com/
-      max_output_size = 17179869184
-      server_store_uri = https://holo.s3.wasabisys.com?local-nar-cache=/var/cache/hydra/nar-cache
-      store_uri = s3://holo?endpoint=s3.wasabisys.com&log-compression=br&ls-compression=br&parallel-compression=1&secret-key=/var/lib/hydra/queue-runner/keys/cache.holo.host-1/secret&write-nar-listing=1
-      upload_logs_to_binary_cache = true
-    '';
     hydraURL = "https://${config.services.nginx.virtualHosts.hydra.serverName}";
     logo = ./logo.png;
     notificationSender = "hydra@holo.host";
@@ -35,9 +27,4 @@
       locations."/".proxyPass = "http://localhost:${toString config.services.hydra.port}";
     };
   };
-
-  systemd.tmpfiles.rules = [
-    "d /var/cache/hydra           0755 hydra hydra -  -"
-    "d /var/cache/hydra/nar-cache 0775 hydra hydra 1d -"
-  ];
 }

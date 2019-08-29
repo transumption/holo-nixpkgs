441f6dafa6a70767bca899cbe803b38d76e67aed
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-29 00:03:55 +0000

    modules/services/holochain-conductor: do not hardcode n3h path

--- modules/services/holochain-conductor.nix
+++ modules/services/holochain-conductor.nix
@@ -44,7 +44,7 @@ in
       ];
 
       preStart = ''
-        mkdir -p ${home}/n3h
+        mkdir -p ${cfg.config.network.n3h_persistence_path}
         cat ${toml} > ${home}/config.toml
         sed -i s/@public_key@/$(cat ${home}/holoportos-key.pub)/ ${home}/config.toml
       '';

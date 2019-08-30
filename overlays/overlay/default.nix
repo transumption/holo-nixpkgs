e662471cbdae6e385894ae4bf9b2a7eafe139207
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-30 17:30:30 +0000

    mkImage: move to overlay

--- overlays/overlay/default.nix
+++ overlays/overlay/default.nix
@@ -55,12 +55,62 @@ in
     inherit (rust.packages.nightly) rustPlatform;
   });
 
-  hAppPackages = recurseIntoAttrs {
-    example-happ = callPackage ./happ-packages/example-happ {};
-    happ-store = callPackage ./happ-packages/happ-store {};
-    holo-hosting-app = callPackage ./happ-packages/holo-hosting-app {};
-    holofuel = callPackage ./happ-packages/holofuel {};
-    servicelogger = callPackage ./happ-packages/servicelogger {};
+  mkImage = profile:
+    let
+      allowCross = config.allowCross or true;
+
+      nixos = import "${pkgs.path}/nixos" {
+        configuration = { config, ... }: {
+	  imports = [ profile ];
+
+	  nixpkgs.localSystem.system = if allowCross
+	    then builtins.currentSystem
+	    else config.nixpkgs.hostPlatform.system;
+	};
+      };
+
+      inherit (nixos.config.system) build;
+      inherit (nixos.config.nixpkgs.hostPlatform) system;
+
+      image = if build ? "vm"
+        then build.vm
+        else if build ? "virtualBoxOVA"
+        then build.virtualBoxOVA
+        else if build ? "sdImage"
+        then build.sdImage
+        else if build ? "isoImage"
+        then build.isoImage
+        else throw "${build} doesn't expose any known image format";
+
+      stopgap = drv: if allowCross
+        then drv
+        else runCommand drv.name {} ''
+          mkdir -p $out
+
+          for f in ${drv}/*; do
+            if [ "$f" = "${drv}/nix-support" ]; then
+              cp -r $f $out
+              chmod -R +w $out/$(basename $f)
+            else
+              cp -rs $f $out
+            fi
+          done
+
+          for f in $out/nix-support/*; do
+            substituteInPlace $f --replace ${drv} $out
+          done
+        '';
+    in
+    lib.recursiveUpdate (stopgap image) {
+      meta.platforms = [ system ];
+    };
+
+  dnaPackages = recurseIntoAttrs {
+    example-happ = callPackage ./dna-packages/example-dna {};
+    happ-store = callPackage ./dna-packages/happ-store {};
+    holo-hosting-app = callPackage ./dna-packages/holo-hosting-app {};
+    holofuel = callPackage ./dna-packages/holofuel {};
+    servicelogger = callPackage ./dna-packages/servicelogger {};
   };
 
   aurora-led = callPackage ./aurora-led {};
--- release.nix
+++ release.nix
@@ -2,58 +2,6 @@
 
 with pkgs;
 
-let
-  mkImage = profile:
-    let
-      allowCross = config.allowCross or true;
-
-      nixos = import "${pkgs.path}/nixos" {
-        configuration = { config, ... }: {
-	  imports = [ profile ];
-
-	  nixpkgs.localSystem.system = if allowCross
-	    then builtins.currentSystem
-	    else config.nixpkgs.hostPlatform.system;
-	};
-      };
-
-      inherit (nixos.config.system) build;
-      inherit (nixos.config.nixpkgs.hostPlatform) system;
-
-      image = if build ? "vm"
-        then build.vm
-        else if build ? "virtualBoxOVA"
-        then build.virtualBoxOVA
-        else if build ? "sdImage"
-        then build.sdImage
-        else if build ? "isoImage"
-        then build.isoImage
-        else throw "${build} doesn't expose any known image format";
-
-      stopgap = drv: if allowCross
-        then drv
-        else runCommand drv.name {} ''
-          mkdir -p $out
-
-          for f in ${drv}/*; do
-            if [ "$f" = "${drv}/nix-support" ]; then
-              cp -r $f $out
-              chmod -R +w $out/$(basename $f)
-            else
-              cp -rs $f $out
-            fi
-          done
-
-          for f in $out/nix-support/*; do
-            substituteInPlace $f --replace ${drv} $out
-          done
-        '';
-    in
-    lib.recursiveUpdate (stopgap image) {
-      meta.platforms = [ system ];
-    };
-in
-
 {
   holoportos = recurseIntoAttrs {
     installers = recurseIntoAttrs {

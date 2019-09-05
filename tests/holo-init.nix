149d420c528c415a0ce6e740d67a84e645a585dc
samrose <samuel.rose@gmail.com>
2019-09-05 10:21:45 -0400

    holo init test hc keygen piece

--- tests/holo-init.nix
+++ tests/holo-init.nix
@@ -4,15 +4,18 @@ with pkgs;
 with import "${pkgs.path}/nixos/lib/testing.nix" { inherit pkgs system; };
 
 makeTest {
-  name = "holo-envoy";
-
+  name = "holo-init";
   machine = {
     imports = [ (import ../profiles/holoportos/demo) ];
+    virtualisation.memorySize = 2024;
   };
 
   testScript = ''
     startAll;
-    $machine->execute("curl localhost");
+    print($machine->execute("pwd"));
+    print($machine->succeed("free -m"));
+    print($machine->succeed("hc keygen -np /var/lib/holochain-conductor/holoportos-key"));
+    print($machine->execute("ls -la /var/lib/holochain-conductor/holoportos-key"));
     $machine->shutdown;
   '';
 

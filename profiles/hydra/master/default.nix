a03b5fed25d2f35df48dd8aa0d05688c6401d45e
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-09-07 04:44:12 +0000

    profiles/hydra/master: use CDN for log_prefix and server_store_uri

--- profiles/hydra/master/default.nix
+++ profiles/hydra/master/default.nix
@@ -3,7 +3,6 @@
 let
   wasabiBucket = "cache.holo.host";
   wasabiEndpoint = "s3.wasabisys.com";
-  wasabiURL = "http://${wasabiBucket}.${wasabiEndpoint}";
 in
 
 {
@@ -50,9 +49,9 @@ in
 
   services.hydra.extraConfig = ''
     binary_cache_public_uri = https://cache.holo.host
-    log_prefix = ${wasabiURL}/
+    log_prefix = https://cache.holo.host/
     max_output_size = 17179869184
-    server_store_uri = ${wasabiURL}?local-nar-cache=/var/cache/hydra/nar-cache
+    server_store_uri = https://cache.holo.host?local-nar-cache=/var/cache/hydra/nar-cache
     store_uri = s3://${wasabiBucket}?endpoint=${wasabiEndpoint}&log-compression=br&ls-compression=br&parallel-compression=1&secret-key=/var/lib/hydra/queue-runner/keys/cache.holo.host-1/secret&write-nar-listing=1
     upload_logs_to_binary_cache = true
   '';

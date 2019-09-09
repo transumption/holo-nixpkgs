63c2499df4c53bb31236035198f8d786ba203af1
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-09-09 23:01:29 +0000

    profiles/holoportos/qemu: remove <nixos-config> handling

--- profiles/holoportos/qemu/default.nix
+++ profiles/holoportos/qemu/default.nix
@@ -2,20 +2,11 @@
 
 let
   nixpkgs = import ../../../vendor/nixpkgs.nix;
-
-  nixos-config =
-    let
-      res = builtins.tryEval <nixos-config>;
-    in
-    if res.success
-    then <nixos-config>
-    else ../.;
 in
 
 {
   imports = [
     "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
-    nixos-config
   ];
 
   environment.loginShellInit = ''

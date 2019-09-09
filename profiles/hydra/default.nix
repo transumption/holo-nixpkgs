861c0314766ea4deeab9b79c37e093058582693b
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-09-09 22:48:08 +0000

    profiles/hydra: lib.mkForce root SSH keys

--- profiles/hydra/default.nix
+++ profiles/hydra/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   imports = [
@@ -20,7 +20,7 @@
 
   # NB! Anyone in this list is in a position to poison binary cache. Give away
   # root or trusted user access to Hydra *very* carefully.
-  users.users.root.openssh.authorizedKeys.keys = [
+  users.users.root.openssh.authorizedKeys.keys = lib.mkForce [
     # Yegor Timoshenko
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLGgzH3ROvo65cnvkXmuz7Qc9bPvU+L2SrafQh0bMrK"
   ];

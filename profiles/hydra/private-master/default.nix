9c38184f47e8b181b0c7634bcca3d463c701e910
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-09-14 13:11:12 +0000

    profiles/hydra/private-master: private = 1

--- profiles/hydra/private-master/default.nix
+++ profiles/hydra/private-master/default.nix
@@ -17,7 +17,12 @@
 
   nix.useSandbox = false;
 
-  services.hydra.hydraURL = "https://private-hydra.holo.host";
+  services.hydra = {
+    hydraURL = "https://private-hydra.holo.host";
+    extraConfig = ''
+      private = 1
+    '';
+  };
 
   services.nginx.virtualHosts.hydra = {
     basicAuthFile = "/etc/nixos/htpasswd";

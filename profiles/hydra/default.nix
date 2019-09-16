72a0a4129919e5bb63d729f9e7fb0d5ffd32939a
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-09-16 14:14:06 +0000

    profiles/hydra/minion: merge into profiles/hydra
    This fixes priority bug introduced in: 861c0314766ea4deeab9b79c37e093058582693b

--- profiles/hydra/default.nix
+++ profiles/hydra/default.nix
@@ -23,7 +23,9 @@
   # NB! Anyone in this list is in a position to poison binary cache. Give away
   # root or trusted user access to Hydra *very* carefully.
   users.users.root.openssh.authorizedKeys.keys = lib.mkForce [
+    # /var/lib/hydra/queue-runner/.ssh/id_ed25519
+    ''command="nix-store --serve --write" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE/SlkjHXwID8sIXRAkpqeB17S3J0ie27MXoVs8BTb5S''
     # Yegor Timoshenko
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLGgzH3ROvo65cnvkXmuz7Qc9bPvU+L2SrafQh0bMrK"
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLGgzH3ROvo65cnvkXmuz7Qc9bPvU+L2SrafQh0bMrK''
   ];
 }

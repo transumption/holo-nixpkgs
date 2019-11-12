16be29ca566119f4c5cc6586233d0ff700b2fdda
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-11-12 12:44:27 +0000

    Revert "Support partial update of api/v1/config via POST, full update via PUT"
    This reverts commit 9ce52826b08a14f71cea8e723f5ec93dd53d1282.
    
    - Requested to make further changes in a separate branch
    - Re-introduces x-hpos-admin-cas in GET /v1/config, for the third time,
      despite lack of consensus and without going through RFC process
      (previously added in 6d931fcefd58ac52fd8f4a1d72dfc2648c5ad1dc and
      reverted in 309ec8b9f8ebcba8a89ed4f00be9d61a498a82a5, then re-added
      in 60bde0f3bc27a67522faf21e8ef4ecb32d16d112 and subsequently
      reverted in fd48a8cd6d16fb70ff127856a85baca92c68173b); also see:
      + https://github.com/Holo-Host/rfcs/pull/2
      + https://github.com/Holo-Host/rfcs/pull/9
    - hpos-state.json is schema-less and defining schema to validate it
      against is invalid
    - Commit messages still do not follow CONTRIBUTING.md guidelines
    - Not PEP8-compliant
    - POST endpoint was added on incorrect assumption that curl doesn't
      support PUT requests (see `curl -X PUT`), partial updates don't
      make any sense given compare-and-swap flow and needlessly complicate
      implementation
    - `logging.basicConfig(level=logging.WARNING)` was re-added, despite
      that being the default

--- overlays/holo-nixpkgs/default.nix
+++ overlays/holo-nixpkgs/default.nix
@@ -175,7 +175,7 @@ in
 
   hpos-admin = callPackage ./hpos-admin {
     stdenv = stdenvNoCC;
-    python3 = python3.withPackages (ps: [ ps.flask ps.gevent ps.schema ]);
+    python3 = python3.withPackages (ps: [ ps.flask ps.gevent ]);
   };
 
   holoportos-install = callPackage ./holoportos-install {};
--- overlays/holo-nixpkgs/hpos-admin/hpos-admin.py
+++ overlays/holo-nixpkgs/hpos-admin/hpos-admin.py
@@ -2,7 +2,6 @@ from base64 import b64encode
 from flask import Flask, jsonify, request
 from gevent import subprocess, pywsgi, queue, spawn, lock
 from hashlib import sha512
-from schema import Schema, And, Use, Optional
 from tempfile import mkstemp
 import json
 import logging
@@ -11,22 +10,18 @@ import os
 app = Flask(__name__)
 log = logging.getLogger(__name__)
 rebuild_queue = queue.PriorityQueue()
-state_lock = lock.Semaphore(1)
+lock = lock.Semaphore(1)
 
 
 def rebuild_worker():
     while True:
-        (pri, cmd) = rebuild_queue.get()
+        (_, cmd) = rebuild_queue.get()
         rebuild_queue.queue.clear()
-        try:
-            log.info(f"Running (priority {pri}): {cmd!r}")
-            subprocess.run(cmd, check=True)
-        except Exception as exc:
-            log.warning(f"Exception: {exc}")
+        subprocess.run(cmd)
 
-def rebuild(*args, priority=1):
-    """Schedule a rebuild, w/ optional args; the highest priority instance waiting gets executed."""
-    rebuild_queue.put((priority, ['sudo', 'nixos-rebuild', 'switch'] + list(args)))
+
+def rebuild(priority, args):
+    rebuild_queue.put((priority, ['sudo', 'nixos-rebuild', 'switch'] + args))
 
 
 def get_state_path():
@@ -45,8 +40,7 @@ def cas_hash(data):
 
 @app.route('/v1/config', methods=['GET'])
 def get_config():
-    config = get_state_data()['v1']['config']
-    return jsonify(config), 200, {'x-hpos-admin-cas': cas_hash(config)}
+    return jsonify(get_state_data()['v1']['config'])
 
 
 def replace_file_contents(path, data):
@@ -56,45 +50,17 @@ def replace_file_contents(path, data):
     os.rename(tmp_path, path)
 
 
-config_schema = Schema({
-    'admin': {
-        'email':  And(str, lambda s: '@' in s),
-        'public_key':  And(str, len),
-    },
-    Optional('name'): And(str, len),
-    Optional('holoportos'): {
-        Optional('network'): And(str, Use(str.lower), lambda s: s in ('live', 'dev', 'test')),
-        Optional('sshAccess'): bool,
-    },
-})
-
-
-def merge(dst, src):
-    """Recursively merges src into dst."""
-    if not isinstance(dst, dict) or not isinstance(src, dict):
-        return src
-    for k in src:
-        if k in dst:
-            dst[k] = merge(dst[k], src[k])
-        else:
-            dst[k] = src[k]
-    return dst
-
-
-@app.route('/v1/config', methods=['PUT','POST'])
+@app.route('/v1/config', methods=['PUT'])
 def put_config():
-    """Replace current config with complete replacement (PUT), or partial update (POST)"""
-    config = request.get_json(force=True)
-    with state_lock:
+    with lock: # Ensure no race condition in checking state CAS hash, to replacing state
         state = get_state_data()
         if request.headers.get('x-hpos-admin-cas') != cas_hash(state['v1']['config']):
             return '', 409
-        if request.method == 'POST':
-            config = merge(state['v1']['config'], config)
-        state['v1']['config'] = config_schema.validate(config)
+        state['v1']['config'] = request.get_json(force=True)
         replace_file_contents(get_state_path(), json.dumps(state, indent=2))
-    rebuild(priority=5)
+    rebuild(priority=5, args=[])
     return '', 200
+
 
 
 def zerotier_info():
@@ -112,11 +78,10 @@ def status():
 
 @app.route('/v1/upgrade', methods=['POST'])
 def upgrade():
-    rebuild('--upgrade', priority=1)
+    rebuild(priority=1, args=['--upgrade'])
     return '', 200
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.WARNING)
     spawn(rebuild_worker)
     pywsgi.WSGIServer(('::1', 5000), app).serve_forever()

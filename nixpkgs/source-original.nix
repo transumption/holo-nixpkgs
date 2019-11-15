a940c2ca20ae33f0a9844492308cdaed27a570ac
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-11-15 18:37:29 +0000

    nixpkgs: bump to 5f506b95f9f6290f4c46f523cc3f5cef581c5666

--- nixpkgs/source-original.nix
+++ nixpkgs/source-original.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/1412af4b2cfae71d447164097d960d426e9752c0.tar.gz";
-  sha256 = "1m2d0cwckblnsv8c4x69h3c6672sxiw4d41jvaiqh76q945dnzpw";
+  url = "https://github.com/NixOS/nixpkgs/archive/5f506b95f9f6290f4c46f523cc3f5cef581c5666.tar.gz";
+  sha256 = "14ry2fs2pny2shlg2dq8c9vns636gv0zvfh0dmyy4bnq713k377n";
 }

e44b625c3794ddaf13e1453d315b02118fa379f0
Perry Kundert <perry@hardconsulting.com>
2019-12-03 15:06:16 -0800

    Further logging, error checking and service start-up work o Add more logging to holo-auth-client.nix script, to determine what   was being done when a failure occurs o Detect missing/empty holo-keystore files (resulting from a prior   failed attempt to run hpos-state-derive-keystore, eg. out of memory) o Adjust the holochain-conductor service to wait until derivation of   keys by holo-auth-client is complete, before starting; it cannot   successfully start without keys o Log around potentially long-running requests such as the email auth o Capture failure of run and/or decode output of zerotier-cli call

--- modules/services/holo-auth-client.nix
+++ modules/services/holo-auth-client.nix
@@ -35,26 +35,33 @@ in
           zerotier-cli -j listnetworks | jq -r .[0].status
         }
 
-        sleep 10
+        echo >&2 "Zerotier status, start: $(zerotier_status)"
 
         while [ "$(zerotier_status)" = "REQUESTING_CONFIGURATION" ]; do
-          echo "Requesting ZeroTier configuration..." >&2
-          sleep 1
+          echo >&2 "Awaiting requested ZeroTier configuration..."
+          sleep 3
         done
 
-        if [ "$(zerotier_status)" = "ACCESS_DENIED" ]; then
-          echo "Running HPOS init..." >&2
+        if [ "$(zerotier_status)" = "ACCESS_DENIED" ] \
+            || [ ! -s holo-keystore ] || [ ! -s holo-keystore.pub ]; then
+          # Not yet authorized w/ Zerotier, or the derived keystore is missing/empty
+          echo >&2 "Running HPOS init..."
           export HPOS_STATE_PATH=$(hpos-init)
 
           mkdir -p /var/lib/holochain-conductor
           cd /var/lib/holochain-conductor
 
-          echo "Deriving Holo keystore..." >&2
-          hpos-state-derive-keystore < $HPOS_STATE_PATH > holo-keystore 2> holo-keystore.pub
+          echo >&2 "Deriving Holo keystore to $HPOS_STATE_PATH/holo-keystore..."
+          if ! hpos-state-derive-keystore < $HPOS_STATE_PATH > holo-keystore 2> holo-keystore.pub; then
+            echo >&2 "Failed to derive keys from $HPOS_STATE_PATH"
+            exit 1
+          fi
           export HOLO_PUBLIC_KEY=$(cat holo-keystore.pub)
-
+          echo >&2 "Authorizing Holo Auth Agent ID: $HOLO_PUBLIC_KEY..."
           exec ${cfg.package}/bin/holo-auth-client
         fi
+
+        echo >&2 "Zerotier status, final: $(zerotier_status)"
       '';
 
       serviceConfig = {

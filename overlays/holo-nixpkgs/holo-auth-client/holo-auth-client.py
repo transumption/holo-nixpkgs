ee7648878c052a4c3d54f513d53f6d18580fba77
Perry Kundert <perry@hardconsulting.com>
2019-12-04 10:25:54 -0800

    Further refinement of testing of Holo Auth failure modes o Check for missign HPOS_STATE_PATH variable o Issue details of Exception type o Always check the exit status of zorotier-cli

--- overlays/holo-nixpkgs/holo-auth-client/holo-auth-client.py
+++ overlays/holo-nixpkgs/holo-auth-client/holo-auth-client.py
@@ -13,12 +13,14 @@ def state_path():
 
 
 def admin_email():
+    path = state_path()
     try:
-        with open(state_path(), 'r') as f:
+        assert path, "Missing HPOS_STATE_PATH environment variable"
+        with open(path, 'r') as f:
             config = json.load(f)
         return config['v1']['config']['admin']['email']
     except Exception as exc:
-        logging.warning(f"Failed to get admin email from HPOS State in: {state_path()}; {exc}")
+        logging.warning(f"Failed to get admin email from HPOS State in: {path}; {exc!r}")
         raise
 
 
@@ -35,10 +37,10 @@ def confirm_email(email, zerotier_address):
 
 def zerotier_address():
     try:
-        proc = subprocess.run(["zerotier-cli", "-j", "info"], capture_output=True)
+        proc = subprocess.run(["zerotier-cli", "-j", "info"], capture_output=True, check=True)
         info = json.loads(proc.stdout)
     except Exception as exc:
-        logging.warning(f"Failed to obtain Zerotier node address: {exc}")
+        logging.warning(f"Failed to obtain Zerotier node address: {exc!r}")
         raise
     return info['address']
 

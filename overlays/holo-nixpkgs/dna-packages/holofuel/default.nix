ce082dd28bccf3b5230d03d933694dc6d02a9b47
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-12-09 17:30:23 +0000

    dnaPackages.holofuel: 0.9.7-alpha1 -> 0.13.1-alpha1
    Co-authored-by: Perry Kundert <perry.kundert@holo.host>

--- overlays/holo-nixpkgs/dna-packages/holofuel/default.nix
+++ overlays/holo-nixpkgs/dna-packages/holofuel/default.nix
@@ -2,9 +2,9 @@
 
 let
   src = fetchurl {
-    url = "https://holo-host.github.io/holofuel/releases/download/0.9.7-alpha1/holofuel.dna.json";
+    url = "https://holo-host.github.io/holofuel/releases/download/v0.13.1-alpha1/holofuel.dna.json";
     name = "holofuel.dna.json";
-    sha256 = "0cgl88izvj4gnjvv21j8fz0fax3l45gmf0iqnhd64qpdgsb2ihah";
+    sha256 = "10xmq52m37r09flmsri64iwrpi9l88s9zmdl50l346ld6rxd0jf8";
   };
 in
 

00c089003fdd4874672e6aec733600483b0676fb
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-12-12 13:10:20 +0000

    servicelogger: bump to 3b716c325e4243f5c88e2f65530cd6495a9f4ae5
    Co-authored-by: Perry Kundert <perry@hardconsulting.com>

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -24,8 +24,8 @@ let
   servicelogger = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "servicelogger";
-    rev = "5b96b9e507a6000b99f2e2473722e94412901990";
-    sha256 = "1if37bwlv32zdqxs0s6zb7bnymw4x6594aj694bhilbkkadg2wxj";
+    rev = "3b716c325e4243f5c88e2f65530cd6495a9f4ae5";
+    sha256 = "03izll3b5ajgbw9b6df7vxc68ysxd4xzbrw2p41r9ybgmnn9bii8";
   };
 in
 

c1e539c8dc54d8191e8051841a2fbdb350729e68
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-12-28 01:56:39 +0000

    logical/self-aware: init Holo Nixpkgs channel

--- profiles/logical/self-aware.nix
+++ profiles/logical/self-aware.nix
@@ -1,3 +1,5 @@
+{ config, lib, pkgs, ... }:
+
 let
   self = "/nix/var/nix/profiles/per-user/root/channels/holo-nixpkgs";
 in
@@ -9,4 +11,14 @@ in
     "nixos-config=/etc/nixos/configuration.nix"
     "/nix/var/nix/profiles/per-user/root/channels"
   ];
+
+  system.activationScripts.nix = lib.mkForce (lib.stringAfter [ "etc" "users" ] ''
+    ${pkgs.nix}/bin/nix ping-store --no-net
+
+    if [ ! -e /root/.nix-channels ]; then
+      echo "${config.system.defaultChannel} holo-nixpkgs" > /root/.nix-channels
+    fi
+  '');
+
+  system.defaultChannel = "https://hydra.holo.host/channel/custom/holo-nixpkgs/master/holo-nixpkgs";
 }

3d7c1a49c775db5bd684bd040d519bbcb89b73e4
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-12-28 14:59:41 +0000

    logical/self-aware: clean up

--- profiles/logical/self-aware.nix
+++ profiles/logical/self-aware.nix
@@ -1,24 +1,24 @@
 { config, lib, pkgs, ... }:
 
 let
-  self = "/nix/var/nix/profiles/per-user/root/channels/holo-nixpkgs";
+  channelsPath = "/nix/var/nix/profiles/per-user/root/channels";
 in
 
 {
   nix.nixPath = [
-    "nixpkgs=${self}/nixpkgs"
-    "nixpkgs-overlays=${self}/overlays"
     "nixos-config=/etc/nixos/configuration.nix"
-    "/nix/var/nix/profiles/per-user/root/channels"
+    "nixpkgs=${channelsPath}/holo-nixpkgs/nixpkgs"
+    "nixpkgs-overlays=${channelsPath}/holo-nixpkgs/overlays"
+    channelsPath
   ];
 
   system.activationScripts.nix = lib.mkForce (lib.stringAfter [ "etc" "users" ] ''
-    ${pkgs.nix}/bin/nix ping-store --no-net
+    ${config.nix.package}/bin/nix ping-store --no-net
 
     if [ ! -e /root/.nix-channels ]; then
-      echo "${config.system.defaultChannel} holo-nixpkgs" > /root/.nix-channels
+      ${config.nix.package}/bin/nix-channel --add ${config.system.defaultChannel} holo-nixpkgs
     fi
   '');
 
-  system.defaultChannel = "https://hydra.holo.host/channel/custom/holo-nixpkgs/master/holo-nixpkgs";
+  system.defaultChannel = lib.mkDefault "https://hydra.holo.host/channel/custom/holo-nixpkgs/master/holo-nixpkgs";
 }

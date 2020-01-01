74f8b70af1bc62366298e85a85c5f5d38688a626
Yegor Timoshenko <yegortimoshenko@transumption.com>
2020-01-01 18:01:16 +0000

    hpos-init: decouple HPOS Seed thread from config scan

--- overlays/holo-nixpkgs/hpos-init/hpos_init/main.py
+++ overlays/holo-nixpkgs/hpos-init/hpos_init/main.py
@@ -1,8 +1,7 @@
-from functools import lru_cache
 from glob import glob
-from twisted.internet import reactor
-from twisted.internet.defer import ensureDeferred
+from twisted.internet import defer, reactor
 from wormhole.errors import WormholeError
+import functools
 import hpos_seed
 import logging
 import os
@@ -13,77 +12,79 @@ import time
 log = logging.getLogger(__name__)
 
 
-@lru_cache()
+@functools.lru_cache()
 def runtime_dir():
     return os.getenv('RUNTIME_DIRECTORY', '.')
 
 
-@lru_cache()
+@functools.lru_cache()
 def state_dir():
     return os.getenv('STATE_DIRECTORY', '.')
 
 
-@lru_cache()
+@functools.lru_cache()
 def runtime_config_path():
     return runtime_dir() + '/hpos-config.json'
 
 
-@lru_cache()
+@functools.lru_cache()
 def state_config_path():
     return state_dir() + '/hpos-config.json'
 
 
-@lru_cache()
+@functools.lru_cache()
 def wormhole_code_path():
     return runtime_dir() + '/wormhole-code.txt'
 
 
-def on_wormhole_code(wormhole_code):
-    with open(wormhole_code_path(), 'w') as f:
-        f.write(wormhole_code)
+def ensureDeferred(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        result = f(*args, **kwargs)
+        return defer.ensureDeferred(result)
+    return wrapper
 
 
-async def receive_config():
-    backoff_exp = 1
-    while True:
-        try:
-            return await hpos_seed.receive(on_wormhole_code, reactor)
-        except WormholeError as e:
-            pass
-        finally:
-            if os.path.exists(wormhole_code_path()):
-                os.remove(wormhole_code_path())
+@ensureDeferred
+async def receive_config(backoff_exp=1):
+    def on_wormhole_code(wormhole_code):
+        with open(wormhole_code_path(), 'w') as f:
+            f.write(wormhole_code)
+    try:
+        config = await hpos_seed.receive(on_wormhole_code, reactor)
+        with open(state_config_path(), 'wb') as f:
+            f.write(config)
+    except WormholeError as e:
         backoff = 2 ** backoff_exp
-        backoff_exp += 1
         (exc_type, _, _) = sys.exc_info()
         log.warning("receive failed with %s, retrying in %d seconds",
                     exc_type, backoff)
-        time.sleep(backoff)
-
-
-async def resolve_config_path():
-    paths = glob(state_config_path()) + glob('/media/*/hpos-config.json')
-    if paths == []:
-        config = await receive_config()
-        with open(state_config_path(), 'wb') as f:
-            f.write(config)
-        return state_config_path()
-    else:
-        return paths[0]
-
-
-async def hpos_init():
-    try:
-        if glob(runtime_config_path()) == []:
-            config_path = await resolve_config_path()
-            os.symlink(config_path, runtime_config_path())
+        reactor.callLater(backoff, receive_config, backoff_exp + 1)
     finally:
-        reactor.callLater(0, reactor.stop)
+        if os.path.exists(wormhole_code_path()):
+            os.remove(wormhole_code_path())
+
+
+def scan_config_paths():
+    return glob(state_config_path()) + glob('/media/*/hpos-config.json')
+
+
+@ensureDeferred
+async def scan_for_config():
+    config_paths = scan_config_paths()
+    if config_paths:
+        try:
+            os.symlink(config_paths[0], runtime_config_path())
+        finally:
+            reactor.callLater(0, reactor.stop)
+    reactor.callLater(1, scan_for_config)
 
 
 def main():
-    ensureDeferred(hpos_init())
-    reactor.run()
+    if glob(runtime_config_path()) == []:
+        reactor.callLater(0, receive_config)
+        reactor.callLater(0, scan_for_config)
+        reactor.run()
 
 
 if __name__ == '__main__':
